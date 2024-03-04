d453f1824473327d9746b2d5d85d08e6bfbe0e86
Binay6 <binay3662@gmail.com>
2024-03-04 11:16:44 +0530

    card component is created

---
+++ 03cardView/components/Card.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const card  = ()=> {
+    return(
+        <>
+        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 rounded-full mx-auto"
+          src="https://th.bing.com/th/id/OIP.WV-7Ok0PuAP_Pj-kHH5l7AHaEo?w=1920&h=1200&rs=1&pid=ImgDetMain"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. Its easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-med">
+            <div>Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
+      <br></br>
+
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 rounded-full mx-auto"
+          src="https://th.bing.com/th/id/OIP.WV-7Ok0PuAP_Pj-kHH5l7AHaEo?w=1920&h=1200&rs=1&pid=ImgDetMain"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. Its easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-med">
+            <div>Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
+      <br></br>
+
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 rounded-full mx-auto"
+          src="https://th.bing.com/th/id/OIP.WV-7Ok0PuAP_Pj-kHH5l7AHaEo?w=1920&h=1200&rs=1&pid=ImgDetMain"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. Its easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-med">
+            <div>Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
+        </>
+    ) ;
+}
+
+export default card
--- 03cardView/src/App.jsx
+++ 03cardView/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "../components/Card.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,28 +13,9 @@ function App() {
         Vite With TailWind
       </h1>
 
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img
-          class="w-24 h-24 rounded-full mx-auto"
-          src="https://th.bing.com/th/id/OIP.WV-7Ok0PuAP_Pj-kHH5l7AHaEo?w=1920&h=1200&rs=1&pid=ImgDetMain"
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div class="pt-6 space-y-4">
-          <blockquote>
-            <p class="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. Its easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption class="font-med">
-            <div>Sarah Dayan</div>
-            <div>Staff Engineer, Algolia</div>
-          </figcaption>
-        </div>
-      </figure>
+      <Card />
+
+      
     </>
   );
 }
